06140156142f922bcadb3895bae6cc91ee962855
Alana Martin <alana@alanamartin.com>
2020-03-08 11:51:39 -0400

    Add Header React Components

--- src/App.js
+++ src/App.js
@@ -6,21 +6,58 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          < Welcome className="welcome" name="Alana" />
+          < Settings className={"settings"} />
+      </header>
+      <player>
+
+      </player>
+
+      <footer>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="credit"
+          href="https://www.alanamartin.com"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+          >
+          Created by Alana Martin
         </a>
-      </header>
+      </footer>
+
+
     </div>
   );
 }
 
 export default App;
+
+class Welcome extends React.Component {
+    render() {
+        return (
+            <div className="welcome">
+                <h1>Simple Chant</h1>
+                < User name={this.props.name}/>
+            </div>
+        );
+    }
+}
+
+class User extends React.Component {
+    render() {
+        return (
+        <p>
+            Hello, {this.props.name ? this.props.name : "Stranger!"}
+        </p>
+        );
+    }
+}
+
+class Settings extends React.Component {
+    render() {
+        return (
+            <div className={this.props.className}>
+               Gear Icon
+            </div>
+        );
+    }
+}
