9a39e9aeb483e9a37213e4c239382e86551c84f2
Alana Martin <alana@alanamartin.com>
2020-03-08 13:04:51 -0400

    Continue Base Build
    Add Tone JS
    Add a basic tone to play
    Add the basic react components
    Add some basic styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
+import gear from './gear-icon.png';
 import './App.css';
+import * as Tone from "tone";
 
 function App() {
   return (
@@ -8,10 +9,12 @@ function App() {
       <header className="App-header">
           < Welcome className="welcome" name="Alana" />
           < Settings className={"settings"} />
+          < Visualizer className={"visualizer"} />
       </header>
-      <player>
-
-      </player>
+      <main id="player">
+          < NowPlaying className={"now-playing"}/>
+          < PlayerControls />
+      </main>
 
       <footer>
         <a
@@ -56,8 +59,123 @@ class Settings extends React.Component {
     render() {
         return (
             <div className={this.props.className}>
-               Gear Icon
+               <img src={gear} />
             </div>
         );
     }
+}
+
+class NowPlaying extends React.Component {
+    render() {
+        return (
+            <section className={this.props.className}>
+                <p>Now Playing Simple Progression</p>
+            </section>
+        );
+    }
+}
+
+
+class Visualizer extends React.Component {
+    render() {
+        return (
+            <section className={this.props.className}>
+                <p>Soon to be visualizer</p>
+            </section>
+        );
+    }
+}
+
+
+class PlayerControls extends React.Component {
+    render() {
+        return (
+            <section className={this.props.className}>
+                < PlayPause />
+                < Tempo />
+                < Looper />
+            </section>
+        );
+    }
+}
+
+class PlayPause extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isPlaying: false};
+        this.state = {buttonImage: false};
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(state => ({
+            isPlaying: !state.isPlaying
+        }));
+        if ( this.state.isPlaying) {
+            playTones();
+        } else {
+            stopStones();
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <button
+                    onClick={this.handleClick}
+                    className={this.props.className}>
+                    Play Pause
+                </button>
+            </div>
+        );
+    }
+}
+
+
+class Tempo extends React.Component {
+    render() {
+        return (
+            <div>
+                <button className={this.props.className}>
+                    Tempo Slider
+                </button>
+            </div>
+        );
+    }
+}
+
+class Looper extends React.Component {
+    render() {
+        return (
+            <div>
+                <button className={this.props.className}>
+                 Looper
+                </button>
+            </div>
+        );
+    }
+}
+
+/** Tone Functions
+ *
+ */
+function playTones(){
+    const time = 0;
+    var synth = new Tone.Synth();
+    synth.toMaster();
+    synth.triggerAttack("C4", time);
+    synth.triggerRelease(time + 1.0);
+
+    var pattern = new Tone.Pattern(function(time, note){
+        synth.triggerAttackRelease(note, 0.25);
+    }, ["C4", "D4", "E4", "G4", "A4"]);
+
+    //begin at the beginning
+    pattern.start(0);
+    Tone.Transport.start();
+}
+
+
+function stopStones(){
+    Tone.Transport.stop();
 }
